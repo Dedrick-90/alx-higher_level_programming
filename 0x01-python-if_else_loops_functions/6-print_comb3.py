8043ab322e3cb5a1a8e0cfcd007e3a339eb01e7b
Dedrick <tdmapani@icloud.com>
2022-12-10 03:09:48 -0800

    1-6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+for digital in range(0, 10):
+    for digit2 in range(digit1 + 1, 10):
+        if digit1 == 8 and digit2 == 9:
+	    print(("{}{}".format(digital1, digit2))
+        else:
+	    print("{}{}".format(digital1, digital2), end=", ")
